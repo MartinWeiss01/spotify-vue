fb8295552c4a7a6fdca24daf2c58fb2495784236
Martin Weiss <jsem@martinweiss.cz>
2023-04-19 20:34:31 +0200

    ci(api): add getTopArtists

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,6 +2,7 @@ import { userEndpoints } from "@/utils/userEndpointsApi";
 import { defineStore } from "pinia";
 import { reactive } from "vue";
 import type { TopTrack } from "@/model/TopTrack";
+import { TopArtist } from "@/model/TopArtist";
 
 
 export const useUserStore = defineStore("user", () => {
@@ -12,9 +13,24 @@ export const useUserStore = defineStore("user", () => {
   ]
 
   const currentTimeRange = reactive(TIME_RANGES[0])
-  const topTracks = reactive({
-    items: <TopTrack[]>[]
+
+  const topArtists = reactive({
+    items: <TopArtist[]>[],
   })
+
+  const topTracks = reactive({
+    items: <TopTrack[]>[],
+  })
+
+  const getTopArtists = async () => {
+    try {
+      const response = await userEndpoints.get(`/me/top/artists?time_range=${currentTimeRange.value}&limit=50`);
+      topArtists.items.push(...response.data.items)
+      console.log(`[useUserStore >> getTopTracks] Top tracks fetched:`, response.data)
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   const getTopTracks = async () => {
     try {
@@ -26,5 +42,5 @@ export const useUserStore = defineStore("user", () => {
     }
   }
 
-  return { TIME_RANGES, currentTimeRange, topTracks, getTopTracks }
+  return { TIME_RANGES, currentTimeRange, topArtists, topTracks, getTopArtists, getTopTracks }
 })
