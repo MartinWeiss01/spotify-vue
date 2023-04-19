a1c41432de2fd819765e186437d5cf9e2a7a6e76
Martin Weiss <jsem@martinweiss.cz>
2023-04-19 21:46:19 +0200

    feat(Stats): show Top Artists

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -4,6 +4,7 @@ import { reactive } from "vue";
 import type { TopTrack } from "@/model/TopTrack";
 import { TopArtist } from "@/model/TopArtist";
 
+type TopGenres = [string, number]
 
 export const useUserStore = defineStore("user", () => {
   const TIME_RANGES = [
@@ -16,6 +17,7 @@ export const useUserStore = defineStore("user", () => {
 
   const topArtists = reactive({
     items: <TopArtist[]>[],
+    genres: <TopGenres[]>[],
   })
 
   const topTracks = reactive({
@@ -26,7 +28,8 @@ export const useUserStore = defineStore("user", () => {
     try {
       const response = await userEndpoints.get(`/me/top/artists?time_range=${currentTimeRange.value}&limit=50`);
       topArtists.items.push(...response.data.items)
-      console.log(`[useUserStore >> getTopTracks] Top tracks fetched:`, response.data)
+      parseUserTaste()
+      console.log(`[useUserStore >> getTopArtists] Top artists fetched:`, response.data)
     } catch (e) {
       console.error(e);
     }
@@ -40,6 +43,15 @@ export const useUserStore = defineStore("user", () => {
     } catch (e) {
       console.error(e);
     }
+  }
+
+  const parseUserTaste = async () => {
+    const genres = topArtists.items.flatMap(el => el.genres)
+    const countedGenres = genres.reduce((acc, el) => {
+      acc[el] = (acc[el] || 0) + 1;
+      return acc;
+    }, {} as Record<string, number>)
+    topArtists.genres.push(...Object.entries(countedGenres).sort((a, b) => b[1] - a[1]))
   }
 
   return { TIME_RANGES, currentTimeRange, topArtists, topTracks, getTopArtists, getTopTracks }
--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -5,22 +5,32 @@ import YourTaste from "@/components/stats/YourTaste.vue";
 import YourTopTracks from "@/components/stats/YourTopTracks.vue";
 import YourTopTrackPlaceholder from "@/components/stats/YourTopTrackPlaceholder.vue";
 import YourTastePlaceholder from "@/components/stats/YourTastePlaceholder.vue";
+import YourTopArtists from "@/components/stats/YourTopArtists.vue";
+import YourTopArtistsPlaceholder from "@/components/stats/YourTopArtistsPlaceholder.vue";
 
 const userStore = useUserStore();
+const topArtists = reactive(userStore.topArtists.items);
 const topTracks = reactive(userStore.topTracks.items);
+const topGenres = reactive(userStore.topArtists.genres);
 
-// Move to StatsView
 if (topTracks.length === 0) {
   userStore.getTopTracks();
+}
+
+if (topArtists.length === 0) {
+  userStore.getTopArtists();
 }
 </script>
 
 <template>
   <div class="flex-column">
+    <YourTopArtists v-if="topArtists.length !== 0" :artists="topArtists" />
+    <YourTopArtistsPlaceholder v-else />
+
     <YourTopTracks v-if="topTracks.length !== 0" :tracks="topTracks" />
     <YourTopTrackPlaceholder v-else />
 
-    <YourTaste v-if="topTracks.length !== 0" />
+    <YourTaste v-if="topArtists.length !== 0" :genres="topGenres" />
     <YourTastePlaceholder v-else />
   </div>
 </template>
