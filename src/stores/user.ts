9f8b56f97d96336d4506436b4ccb47a700186384
Martin Weiss <jsem@martinweiss.cz>
2023-04-19 19:30:31 +0200

    ci(api): add getTopTracks
    Prepare function for fetching user's top 50 tracks

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,7 +1,30 @@
 import { userEndpoints } from "@/utils/userEndpointsApi";
 import { defineStore } from "pinia";
+import { reactive } from "vue";
+import type { TopTrack } from "@/model/TopTrack";
+
 
 export const useUserStore = defineStore("user", () => {
+  const TIME_RANGES = [
+    { value: "short_term", label: "4 weeks" },
+    { value: "medium_term", label: "6 months" },
+    { value: "long_term", label: "All time" },
+  ]
 
-  return {}
+  const currentTimeRange = reactive(TIME_RANGES[0])
+  const topTracks = reactive({
+    items: <TopTrack[]>[]
+  })
+
+  const getTopTracks = async () => {
+    try {
+      const response = await userEndpoints.get(`/me/top/tracks?time_range=${currentTimeRange.value}&limit=50`);
+      topTracks.items.push(...response.data.items)
+      console.log(`[useUserStore >> getTopTracks] Top tracks fetched:`, response.data)
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return { TIME_RANGES, currentTimeRange, topTracks, getTopTracks }
 })
