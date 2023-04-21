b93d66fd5faa628c1226150ebc4f072029a00764
Martin Weiss <jsem@martinweiss.cz>
2023-04-21 19:08:18 +0200

    refactor: improve reactivity & code clean up

--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useUserStore } from "@/stores/user";
-import { reactive, ref } from "vue";
+import { reactive } from "vue";
 import YourTaste from "@/components/stats/YourTaste.vue";
 import YourTopTracks from "@/components/stats/YourTopTracks.vue";
 import YourTastePlaceholder from "@/components/stats/YourTastePlaceholder.vue";
@@ -8,25 +8,18 @@ import YourTopArtists from "@/components/stats/YourTopArtists.vue";
 import MediaItemSkeleton from "@/components/stats/MediaItemSkeleton.vue";
 import DateToggleButton from "@/components/DateToggleButton.vue";
 
-import type { TopTrack } from "@/model/TopTrack";
-import type { TopArtist } from "@/model/TopArtist";
-import { vModelCheckbox } from "vue";
-import { nextTick } from "vue";
-
 const userStore = useUserStore();
-const topArtists = reactive(userStore.topArtists.items); // vue for whatever reason refuses to update data with these vars without hot updating the server
-const topTracks = reactive(userStore.topTracks.items);
-const topGenres = reactive(userStore.topArtists.genres);
 const time_ranges = reactive(userStore.TIME_RANGES);
 //const currentTimeRa = ref(userStore.currentTimeRange.value)
 
-if (topArtists.length === 0) {
+if (userStore.topArtists.items.length === 0) {
   userStore.getTopTracks();
 }
 
-if (topTracks.length === 0) {
+if (userStore.topTracks.items.length === 0) {
   userStore.getTopArtists();
 }
+
 const changeTimeRange = (index: number) => {
   //console.log(`curTimeRange Before ${userStore.currentTimeRange.value}`)
   if (userStore.TIME_RANGES[index] != userStore.currentTimeRange) {
@@ -49,7 +42,7 @@ const changeTimeRange = (index: number) => {
     <!-- TODO fix Toggle Button  -->
     <div class="mt-10">
       <YourTopArtists
-        v-if="topArtists.length !== 0"
+        v-if="userStore.topArtists.items.length !== 0"
         :artists="userStore.topArtists.items"
       />
       <MediaItemSkeleton title="Your Top Artists" v-else />
@@ -57,14 +50,17 @@ const changeTimeRange = (index: number) => {
 
     <div class="mt-10">
       <YourTopTracks
-        v-if="topTracks.length !== 0"
+        v-if="userStore.topTracks.items.length !== 0"
         :tracks="userStore.topTracks.items"
       />
       <MediaItemSkeleton title="Your Top Tracks" v-else />
     </div>
 
     <div class="mt-10 mb-10">
-      <YourTaste v-if="topArtists.length !== 0" :genres="topGenres" />
+      <YourTaste
+        v-if="userStore.topArtists.items.length !== 0"
+        :genres="userStore.topArtists.genres"
+      />
       <YourTastePlaceholder v-else />
     </div>
   </div>
