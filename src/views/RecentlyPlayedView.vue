6dd4692defe186e61ccc468bc446b27894b49228
xkellner <xkellner@mendelu.cz>
2023-04-30 21:26:24 +0200

    RecentlyPlayedView content

--- src/views/RecentlyPlayedView.vue
+++ src/views/RecentlyPlayedView.vue
@@ -1,5 +1,38 @@
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user';
+import { onMounted } from 'vue';
+
+
+
+
+const userStore = useUserStore();
+
+
+
+onMounted(() =>{
+  userStore.getRecentlyPlayed();
+})
+
+console.log(userStore.recentlyPlayed.items[1].track.name)
+
+
+
+
+
+
+</script>
+
+
 <template>
   <div class="pa-4">
-    <span> Recently Played View Content </span>
+    <span> <h1>Your recently played songs:</h1> </span>
+  </div>
+
+  <div class="mt-10">
+    <ul>
+    <li v-for="(track, index) in userStore.recentlyPlayed.items" :key="index">
+      {{ track.track.name }} by {{ track.track.artists[0].name }}
+    </li>
+  </ul>
   </div>
 </template>
